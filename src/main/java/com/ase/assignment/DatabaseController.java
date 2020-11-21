b298dad2e821a566e367db3be735016f87b04dde
irham98 <48781295+irham98@users.noreply.github.com>
2020-11-21 19:17:07 +0800

    changed local MySQL server connection to remote

--- src/main/java/com/ase/assignment/DatabaseController.java
+++ src/main/java/com/ase/assignment/DatabaseController.java
@@ -9,14 +9,15 @@ public class DatabaseController {
      * If using docker-for-windows or docker-for-mac, host should be "host.docker.internal"!
      * Otherwise, it should be whatever IP address your host machine is.
      */
-    private static final String DATABASE_HOST = "host.docker.internal";
+    private static final String DATABASE_HOST = "52.221.188.94";
     private static final String DATABASE_PORT = "3306";
     private static final String DATABASE_SCHEMA = "ase";
     private static final String DATABASE_USERNAME = "root";
-    private static final String DATABASE_PASSWORD = "12345678";
+    private static final String DATABASE_PASSWORD = "1234";
 
     public static Connection getDatabaseConnection() {
         String URL = "jdbc:mysql://" + DATABASE_HOST + ":" + DATABASE_PORT + "/" + DATABASE_SCHEMA;
+
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
